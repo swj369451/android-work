9ca38a3bf17e2dcc042f1b9d82bbd7b006c8d619
swj369451 <swj369451@163.com>
2019-12-28 11:37:35 +0800

    修改了butterknife和githup文件有冲突

--- app/src/main/java/com/qq149/android_work_sm_130/app/MainActivity.java
+++ app/src/main/java/com/qq149/android_work_sm_130/app/MainActivity.java
@@ -1,15 +1,30 @@
 package com.qq149.android_work_sm_130.app;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.FrameLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import com.qq149.android_work_sm_130.R;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+
+
+    @Bind(R.id.frameLayout)
+    FrameLayout frameLayout;
+    @Bind(R.id.rg_main)
+    RadioGroup rgMain;
+    private RadioGroup rm_main;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
     }
 }
